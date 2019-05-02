cc71f8ef884304ab6fee1872c0852f0e2b11d3f4
Mike Cherry <mcherry@zamphear.net>
2019-05-01 17:39:28 -0700

    Added logic to re-order admins

--- ui/src/view/components/LeaderBoard/index.js
+++ ui/src/view/components/LeaderBoard/index.js
@@ -12,30 +12,36 @@ export default function LeaderBoard(props) {
     return null;
   }
 
+  // filter out any ignored users
+  const scoredUsers = users.filter(user => {
+    return !user.attributes.ignore;
+  });
+
+  const ignoredUsers = users.filter(user => {
+    return user.attributes.ignore;
+  });
+
   // sort users by score
   const compare = (a, b) => {
     if (a.attributes.score > b.attributes.score) return -1;
     if (b.attributes.score > a.attributes.score) return 1;
     return 0;
   }
-  users.sort(compare);
+  scoredUsers.sort(compare);
 
-  const [ first, second, third ] = users;
-
+  // check for ties
+  const [first, second, third] = scoredUsers;
   // 3 way tie - show 3rd place style
   if (first.attributes.score === third.attributes.score) {
     first.status = second.status = third.status = styles.third;
-
     // tie first and second - show 2nd and 3rd place style
   } else if (first.attributes.score === second.attributes.score) {
     first.status = second.status = styles.second;
     third.status = styles.third;
-
     // tie second and third - show 1st and 3rd place style
   } else if (second.attributes.score === third.attributes.score) {
     first.status = styles.first;
     second.status = third.status = styles.third;
-
   }
 
   let rootClassName = classnames(styles.root, className);
@@ -45,7 +51,7 @@ export default function LeaderBoard(props) {
       <table>
         <tbody>
           {
-            users.map((user, i) => {
+            scoredUsers.map((user, i) => {
               let rowClassName;
               switch (i) {
                 case 0:
@@ -58,7 +64,7 @@ export default function LeaderBoard(props) {
                   rowClassName = user.style || styles.third;
                   break;
                 default:
-                  rowClassName = '';
+                  rowClassName = styles.last;
                   break;
               }
               return (
@@ -69,6 +75,19 @@ export default function LeaderBoard(props) {
               )
             })
           }
+          {
+            ignoredUsers.map((user, i) => {
+              return (
+                <tr key={i} className={styles.last}>
+                  <th>*{user.name}</th>
+                  <td>{user.attributes.score || 0}</td>
+                </tr>
+              )
+            })
+          }
+          <tr className={styles.footer}>
+              <td colSpan="2">* Admin - Not in contention</td>
+          </tr>
         </tbody>
       </table>
     </Panel>
