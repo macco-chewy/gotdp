43f5d15533ab56e207c2abcfd55c0e529393eff7
Mike Cherry <mcherry@zamphear.net>
2019-05-02 01:39:41 -0700

    Fixed leaderboard status calculation

--- ui/src/view/components/LeaderBoard/index.js
+++ ui/src/view/components/LeaderBoard/index.js
@@ -29,20 +29,71 @@ export default function LeaderBoard(props) {
   }
   scoredUsers.sort(compare);
 
-  // check for ties
-  const [first, second, third] = scoredUsers;
-  // 3 way tie - show 3rd place style
-  if (first.attributes.score === third.attributes.score) {
-    first.status = second.status = third.status = styles.third;
-    // tie first and second - show 2nd and 3rd place style
-  } else if (first.attributes.score === second.attributes.score) {
-    first.status = second.status = styles.second;
-    third.status = styles.third;
-    // tie second and third - show 1st and 3rd place style
-  } else if (second.attributes.score === third.attributes.score) {
-    first.status = styles.first;
-    second.status = third.status = styles.third;
-  }
+  // // check for ties
+  // const [first, second, third] = scoredUsers;
+  // // 3 way tie - show 3rd place style
+  // if (first.attributes.score === third.attributes.score) {
+  //   first.status = second.status = third.status = styles.third;
+  //   // tie first and second - show 2nd and 3rd place style
+  // } else if (first.attributes.score === second.attributes.score) {
+  //   first.status = second.status = styles.second;
+  //   third.status = styles.third;
+  //   // tie second and third - show 1st and 3rd place style
+  // } else if (second.attributes.score === third.attributes.score) {
+  //   first.status = styles.first;
+  //   second.status = third.status = styles.third;
+  // }
+
+  let placements = 0;
+  scoredUsers.map((user, i) => {
+    if (placements <= i) {
+
+      // if there are still leader slots
+      if (placements < 3) {
+        // get current user score
+        const score = user.attributes.score;
+
+        // find all users with same score
+        const sameScores = [user];
+        let x = i + 1;
+        while (x < scoredUsers.length) {
+          const nextUser = scoredUsers[x];
+          const nextScore = nextUser.attributes.score;
+          if (nextScore !== score) {
+            break;
+          }
+          sameScores.push(nextUser);
+          x++;
+        }
+
+        if (placements === 0) {
+          if (sameScores.length === 1) {
+            sameScores.map(user => user.style = styles.first);
+          } else if (sameScores.length === 2) {
+            sameScores.map(user => user.style = styles.second);
+          } else {
+            sameScores.map(user => user.style = styles.third);
+          }
+
+        } else if (placements === 1) {
+          if (sameScores.length === 1) {
+            sameScores.map(user => user.style = styles.second);
+          } else {
+            sameScores.map(user => user.style = styles.third);
+          }
+        } else {
+          sameScores.map(user => user.style = styles.third);
+        }
+
+        placements += sameScores.length;
+
+        // just assign last
+      } else {
+        user.style = styles.last;
+      }
+    }
+
+  });
 
   let rootClassName = classnames(styles.root, className);
 
@@ -52,23 +103,9 @@ export default function LeaderBoard(props) {
         <tbody>
           {
             scoredUsers.map((user, i) => {
-              let rowClassName;
-              switch (i) {
-                case 0:
-                  rowClassName = user.style || styles.first;
-                  break;
-                case 1:
-                  rowClassName = user.style || styles.second;
-                  break;
-                case 2:
-                  rowClassName = user.style || styles.third;
-                  break;
-                default:
-                  rowClassName = styles.last;
-                  break;
-              }
+              console.log(user.style);
               return (
-                <tr key={i} className={rowClassName}>
+                <tr key={i} className={user.style}>
                   <th>{user.name}</th>
                   <td>{user.attributes.score || 0}</td>
                 </tr>
